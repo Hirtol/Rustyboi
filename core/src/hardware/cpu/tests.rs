773dbe2254050cd7de8c727f97c71d8b445871db
Valentijn Hol <valentijnhol@hotmail.nl>
2020-08-15 22:37:16 +0200

    Fix tests

--- core/src/hardware/cpu/tests.rs
+++ core/src/hardware/cpu/tests.rs
@@ -1,6 +1,6 @@
 use crate::hardware::cpu::instructions::{Instruction, RegistryTarget};
 use crate::hardware::cpu::CPU;
-use crate::hardware::registers::Flags;
+use crate::hardware::registers::{Flags, Reg8};
 
 #[test]
 fn test_add() {
@@ -9,14 +9,14 @@ fn test_add() {
     cpu.registers.a = 10;
     cpu.registers.c = 20;
 
-    cpu.execute(Instruction::ADD(RegistryTarget::C));
+    cpu.add(Reg8::C);
 
     assert_eq!(cpu.registers.a, 30);
 
     // Test overflow
     cpu.registers.c = 230;
 
-    cpu.execute(Instruction::ADD(RegistryTarget::C));
+    cpu.add(Reg8::C);
 
     assert_eq!(cpu.registers.a, 4);
     assert!(cpu.registers.f.contains(Flags::CF));
