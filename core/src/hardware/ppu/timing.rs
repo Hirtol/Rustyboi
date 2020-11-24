9a5a866e275c2058a52dd34b434b09abade54013
Valentijn Hol <valentijnhol@hotmail.nl>
2020-11-24 22:21:54 +0100

    Make the mid scanline method slightly more readable.

--- core/src/hardware/ppu/timing.rs
+++ core/src/hardware/ppu/timing.rs
@@ -1,12 +1,14 @@
-use crate::hardware::ppu::{PPU, is_sprite_on_scanline};
-use crate::hardware::ppu::register_flags::LcdControl;
-use crate::scheduler::{Scheduler, Event, EventType};
-use crate::io::interrupts::Interrupts;
 use std::env::Args;
-use crate::hardware::mmu::Memory;
-use crate::hardware::ppu::Mode::LcdTransfer;
+
 use itertools::Itertools;
+
+use crate::hardware::mmu::Memory;
+use crate::hardware::ppu::{is_sprite_on_scanline, PPU};
+use crate::hardware::ppu::Mode::LcdTransfer;
+use crate::hardware::ppu::register_flags::LcdControl;
 use crate::hardware::ppu::tiledata::SpriteAttribute;
+use crate::io::interrupts::Interrupts;
+use crate::scheduler::{Event, EventType, Scheduler};
 
 pub const SCANLINE_DURATION: u64 = 456;
 pub const BASE_LCD_TRANSFER_DURATION: u64 = 172;
@@ -14,7 +16,6 @@ pub const BASE_HBLANK_DURATION: u64 = 204;
 pub const OAM_SEARCH_DURATION: u64 = 80;
 
 impl PPU {
-
     #[inline]
     pub fn get_lcd_transfer_duration(&mut self) -> u64 {
         self.current_lcd_transfer_duration = self.calculate_lcd_transfer_duration();
@@ -60,6 +61,7 @@ impl PPU {
     ///
     /// Since CGB palettes are locked during mode 3 we don't have to worry about those mid-scanline
     /// writes.
+    #[inline]
     pub fn handle_mid_scanline_palette(&mut self, scheduler: &mut Scheduler) {
         if self.get_current_mode() != LcdTransfer || self.cgb_rendering {
             return;
@@ -74,44 +76,48 @@ impl PPU {
         let mut current_scanline = self.scanline_buffer.clone();
         let mut pixels_drawn = (cycles_passed.saturating_sub(12 + to_skip)) as usize;
 
-        // If there are no special events (sprites or window) then it's safe to assume
-        // 1 cycle == 1 pixel. Otherwise we do everything down below.
-        if self.current_lcd_transfer_duration-to_skip != 172 {
-            //TODO: Fix the fact that mid-scanline writes to scroll x or window will have effects here
-            // and cause crashing in Road Rash if we didn't use min(159, value).
-            let mut actual_pixels_drawn = 0;
-            let mut cycles_to_go = pixels_drawn;
-            let window_trigger = self.window_triggered && self.window_x < 168 && self.lcd_control.contains(LcdControl::WINDOW_DISPLAY);
-            let window_x = if window_trigger { (self.window_x as i16).saturating_sub(7) } else { 255 };
-            let mut sprites_to_draw = self.get_drawable_sprites().collect_vec();
-
-            while cycles_to_go > 0 {
-                if let Some(sprite) = sprites_to_draw.last() {
-                    if sprite.x_pos >= actual_pixels_drawn {
-                        cycles_to_go = cycles_to_go.saturating_sub(6);
-                        sprites_to_draw.pop();
-                    }
-                }
-
-                actual_pixels_drawn += 1;
-                cycles_to_go = cycles_to_go.saturating_sub(1);
-            }
-
-            if actual_pixels_drawn as i16 >= window_x {
-                actual_pixels_drawn -= actual_pixels_drawn.saturating_sub(6);
-            }
-
-            pixels_drawn = std::cmp::min(actual_pixels_drawn as usize, 159);
+        // Only if there are sprites or a window on the current scanline do we care about the below.
+        // Those are the only things that would affect mid-scanline timings.
+        if self.current_lcd_transfer_duration - to_skip != 172 {
+            self.sprite_or_window_calculations(&mut pixels_drawn);
         }
 
         self.draw_scanline();
         self.scanline_buffer[..pixels_drawn].swap_with_slice(&mut current_scanline[..pixels_drawn]);
     }
 
+    #[inline]
+    fn sprite_or_window_calculations(&mut self, pixels_drawn: &mut usize) {
+        //TODO: Fix the fact that mid-scanline writes to scroll x or window will have effects here
+        // and cause crashing in Road Rash if we didn't use min(159, value).
+        let mut actual_pixels_drawn = 0;
+        let mut cycles_to_go = *pixels_drawn;
+        let window_trigger = self.window_triggered && self.window_x < 168 && self.lcd_control.contains(LcdControl::WINDOW_DISPLAY);
+        let window_x = if window_trigger { (self.window_x as i16).saturating_sub(7) } else { 255 };
+        let mut sprites_to_draw = self.get_drawable_sprites().collect_vec();
+
+        while cycles_to_go > 0 {
+            if let Some(sprite) = sprites_to_draw.last() {
+                if sprite.x_pos >= actual_pixels_drawn {
+                    cycles_to_go = cycles_to_go.saturating_sub(6);
+                    sprites_to_draw.pop();
+                }
+            }
+
+            actual_pixels_drawn += 1;
+            cycles_to_go = cycles_to_go.saturating_sub(1);
+        }
+
+        if actual_pixels_drawn as i16 >= window_x {
+            actual_pixels_drawn -= actual_pixels_drawn.saturating_sub(6);
+        }
+
+        *pixels_drawn = std::cmp::min(actual_pixels_drawn as usize, 159);
+    }
+
     fn get_drawable_sprites(&self) -> impl Iterator<Item=&SpriteAttribute> {
         let y_size: u8 = if self.lcd_control.contains(LcdControl::SPRITE_SIZE) { 16 } else { 8 };
-        self
-            .oam
+        self.oam
             .iter()
             .filter(|sprite| {
                 let screen_y_pos = sprite.y_pos as i16 - 16;
@@ -121,5 +127,4 @@ impl PPU {
             .sorted_by_key(|x| x.x_pos)
             .rev()
     }
-
 }
