4d2052350194afb4f754ad6cef57ff02d797c1aa
Valentijn Hol <valentijnhol@hotmail.nl>
2020-11-24 22:16:25 +0100

    Move the mid scanline write handling to timing.

--- core/src/hardware/ppu/memory_binds.rs
+++ core/src/hardware/ppu/memory_binds.rs
@@ -7,6 +7,7 @@ use crate::print_array_raw;
 use crate::scheduler::EventType::{DMATransferComplete, HBLANK, VBLANK};
 
 use super::*;
+use crate::hardware::ppu::timing::BASE_LCD_TRANSFER_DURATION;
 
 pub const LCD_CONTROL_REGISTER: u16 = 0xFF40;
 pub const LCD_STATUS_REGISTER: u16 = 0xFF41;
@@ -156,70 +157,6 @@ impl PPU {
             // Ignore writes if they're not valid
             _ => {}
         }
-    }
-
-    /// Emulate a pixel FIFO by synchronising the state after a mid-scanline write.
-    /// If that happens we'll just rerender the scanline from the point we should've been at
-    /// in this clock-cycle, with the new palette in place.
-    ///
-    /// Since CGB palettes are locked during mode 3 we don't have to worry about those mid-scanline
-    /// writes.
-    pub fn handle_mid_scanline_palette(&mut self, scheduler: &mut Scheduler) {
-        if self.get_current_mode() != LcdTransfer || self.cgb_rendering {
-            return;
-        }
-        let cycles_passed = scheduler.current_time - self.latest_lcd_transfer_start;
-        // First 12 cycles are ignored.
-        if cycles_passed <= 12 {
-            self.draw_scanline();
-            return;
-        }
-        let to_skip = (self.scroll_x % 8) as u64;
-        let mut current_scanline = self.scanline_buffer.clone();
-        let mut pixels_drawn = (cycles_passed.saturating_sub(12 + to_skip)) as usize;
-
-        // If there are no special events (sprites or window) then it's safe to assume
-        // 1 cycle == 1 pixel. Otherwise we do everything down below.
-        if self.current_lcd_transfer_duration-to_skip != 172 {
-            let mut actual_pixels_drawn = 0;
-            let mut cycles_to_go = pixels_drawn;
-            let window_trigger = self.window_triggered && self.window_x < 168 && self.lcd_control.contains(LcdControl::WINDOW_DISPLAY);
-            let window_x = if window_trigger { self.window_x.saturating_sub(8) } else { 255 };
-            let mut sprites_to_draw = self.get_drawable_sprites().collect_vec();
-
-            while cycles_to_go > 0 {
-                if actual_pixels_drawn == window_x {
-                    cycles_to_go = cycles_to_go.saturating_sub(6);
-                }
-                if let Some(sprite) = sprites_to_draw.last() {
-                    if sprite.x_pos >= actual_pixels_drawn {
-                        cycles_to_go = cycles_to_go.saturating_sub(6);
-                        sprites_to_draw.pop();
-                    }
-                }
-
-                actual_pixels_drawn += 1;
-                cycles_to_go = cycles_to_go.saturating_sub(1);
-            }
-            pixels_drawn = actual_pixels_drawn as usize;
-        }
-
-        self.draw_scanline();
-        self.scanline_buffer[..pixels_drawn].swap_with_slice(&mut current_scanline[..pixels_drawn]);
-    }
-
-    fn get_drawable_sprites(&self) -> impl Iterator<Item=&SpriteAttribute> {
-        let y_size: u8 = if self.lcd_control.contains(LcdControl::SPRITE_SIZE) { 16 } else { 8 };
-        self
-            .oam
-            .iter()
-            .filter(|sprite| {
-                let screen_y_pos = sprite.y_pos as i16 - 16;
-                is_sprite_on_scanline(self.current_y as i16, screen_y_pos, y_size as i16)
-            })
-            .take(10) // Max 10 sprites per scanline
-            .sorted_by_key(|x| x.x_pos)
-            .rev()
     }
 
     /// Can always access vram if PPU is disabled (then `Mode` == `Hblank`, so allowed).
